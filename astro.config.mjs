b5edd693d1010ddb9e1a1be0289217609edd3554
Kang Jiaming <jiaming_kang@outlook.com>
2024-01-26 12:22:45 +0800

    feat: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,31 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nomy.vercel.app",
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'github-dark-dimmed'
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "github-dark-dimmed",
+      },
+      gfm: true,
+    }),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
     },
-    gfm: true
-  }), sitemap(), react(), tailwind({
-    applyBaseStyles: false
-  })],
-  adapter: vercel(),
-  output: "server"
+  }),
+  output: "server",
 });
